d705fe26c98500a8c2317fb179d680bf0cfe4a46
Weellyyy <wildanprtm2304@gmail.com>
2024-06-13 18:30:28 +0700

    melengkapi command

--- BinarySearch Tree/BinarySearch Tree.cpp
+++ BinarySearch Tree/BinarySearch Tree.cpp
@@ -32,7 +32,7 @@ public:
 	{
 		Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
 		newNode->info = element; //Assign value to the data field of the new data
-		newNode->leftchild = nullptr; // Make the left child of the new node point
-		newNode->rightchild = nullptr; // Make the right child of the new node point
+		newNode->leftchild = nullptr; // Make the left child of the new node point to the null
+		newNode->rightchild = nullptr; // Make the right child of the new node point to the null
 	}
 };
