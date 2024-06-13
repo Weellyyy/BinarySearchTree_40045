a0cfd5e3a2bba0ca82da03923930aa92aa0a59ee
Weellyyy <wildanprtm2304@gmail.com>
2024-06-13 18:29:18 +0700

    Make the left child of the new node point

--- BinarySearch Tree/BinarySearch Tree.cpp
+++ BinarySearch Tree/BinarySearch Tree.cpp
@@ -32,5 +32,6 @@ public:
 	{
 		Node* newNode = new Node(element, nullptr, nullptr); //allocate memory for the new node
 		newNode->info = element; //Assign value to the data field of the new data
+		newNode->leftchild = nullptr; // Make the left child of the new node point
 	}
 };
